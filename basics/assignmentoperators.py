55cd56de9f14191c7328ec795ff70e6bbf2f1551
pbpra <yaryaprotikbose@gmail.com>
2023-10-08 18:56:24 -0400

    First Commit for assignmentoperators.py

---
+++ basics/assignmentoperators.py
@@ -0,0 +1,38 @@
+# Assignment Operators in Python
+
+# Assignment operator (=) assigns a value to a variable
+a = 125
+print('a =', a)  # Output: a = 125
+
+# Addition assignment operator (+=)
+a += 10  # a = a + 10
+print('New value =', a)  # Output: New value = 135
+
+# Subtraction assignment operator (-=)
+a -= 10  # a = a - 10
+print('New value =', a)  # Output: New value = 125
+
+# Multiplication assignment operator (*=)
+a *= 10  # a = a * 10
+print('New value =', a)  # Output: New value = 1250
+
+# Division assignment operator (/=)
+a /= 10  # a = a / 10
+print('New value =', a)  # Output: New value = 125.0
+
+# Floor Division assignment operator (//=)
+a //= 3  # a = a // 3
+print('New value =', a)  # Output: New value = 41.0
+
+# Modulus assignment operator (%=)
+a %= 3  # a = a % 3
+print('New value =', a)  # Output: New value = 2.0
+
+# Exponentiation assignment operator (**=)
+a **= 10  # a = a^10
+print('New value =', a)  # Output: New value = 1024.0
+
+# Multiple assignments in a single line
+a = b = c = 10
+print(a, b, c)  # Output: 10 10 10
+
