d32e646656c67a3abf702859190aad74dba323ca
pbpra <yaryaprotikbose@gmail.com>
2023-10-08 18:48:05 -0400

    First Commit for arithmeticoperators.py

---
+++ basics/arithmeticoperators.py
@@ -0,0 +1,37 @@
+# Arithmetic Operators in Python
+
+# Addition
+x = 10
+y = 5
+addition_result = x + y
+print('Addition:', addition_result)  # Output: Addition: 15
+
+# Subtraction
+x = 10
+y = 5
+subtraction_result = x - y
+print('Subtraction:', subtraction_result)  # Output: Subtraction: 5
+
+# Multiplication
+x = 10
+y = 5
+multiplication_result = x * y
+print('Multiplication:', multiplication_result)  # Output: Multiplication: 50
+
+# Exponentiation (Power)
+x = 2
+y = 3
+exponent_result = x ** y
+print('Exponent:', exponent_result)  # Output: Exponent: 8
+
+# Division
+x = 10
+y = 3
+division_result = x / y
+print('Division:', division_result)  # Output: Division: 3.3333333333333335
+
+# Floor Division (Integer Division)
+x = 10
+y = 3
+floor_division_result = x // y
+print('Floor Division:', floor_division_result)  # Output: Floor Division: 3
