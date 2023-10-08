204c91424239ea8cb50a4b34b95fd6331bf25e86
pbpra <yaryaprotikbose@gmail.com>
2023-10-08 18:58:54 -0400

    First Commit for comparisonoperators.py

---
+++ basics/comparisonoperators.py
@@ -0,0 +1,55 @@
+# Comparison Operators in Python
+
+# Comparison operators are used to compare values and return a Boolean result.
+
+# Equal to (==)
+x = 11
+y = 15
+print('Equals:', x == y)  # Output: Equals: False
+
+# Not equal to (!=)
+x = 11
+y = 15
+print('Not Equals:', x != y)  # Output: Not Equals: True
+
+# Greater than (>)
+x = 11
+y = 15
+print('Greater Than:', x > y)  # Output: Greater Than: False
+
+# Less than (<)
+x = 11
+y = 15
+print('Less Than:', x < y)  # Output: Less Than: True
+
+# Greater than or equal to (>=)
+x = 11
+y = 15
+print('Greater Than Equal to:', x >= y)  # Output: Greater Than Equal to: False
+
+# Less than or equal to (<=)
+x = 11
+y = 15
+print('Less Than Equal to:', x <= y)  # Output: Less Than Equal to: True
+
+# Additional Comparison Examples:
+
+# Comparing strings
+str1 = "apple"
+str2 = "banana"
+print('String Comparison:', str1 < str2)  # Output: String Comparison: True
+
+# Comparing lists
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+print('List Comparison:', list1 != list2)  # Output: List Comparison: True
+
+# Comparing tuples
+tuple1 = (10, 20, 30)
+tuple2 = (5, 10, 15)
+print('Tuple Comparison:', tuple1 >= tuple2)  # Output: Tuple Comparison: True
+
+# Comparing boolean values
+bool1 = True
+bool2 = False
+print('Boolean Comparison:', bool1 == bool2)  # Output: Boolean Comparison: False
